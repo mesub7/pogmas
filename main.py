e13677cc7d8b80819a11835fcf27fa8bd7ee5b9b
David <49169805+mesub7@users.noreply.github.com>
2020-09-10 20:23:36 +0100

    Update main.py
    Added about command.

--- main.py
+++ main.py
@@ -9,18 +9,33 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
+# Just before starting
+
+print('------')
+print('Attempting to connect to discord...')
+
+# The other sutff
 token = open("token.txt","r").readline()
 bot = commands.Bot(command_prefix='p')
 
-@bot.command()
+@bot.command(description='Pings the bot.')
 async def ping(ctx):
     start = time.perf_counter()
     message = await ctx.send("Ping...")
     end = time.perf_counter()
     duration = (end - start) * 1000
     await message.edit(content='Pong! {:.2f}ms'.format(duration))
-    
 
+
+@bot.command(description='Some info about the bot.')
+async def about(ctx):
+    embed = discord.Embed(title="Pogmas", colour=discord.Colour(0x38fe), description="A simple discord bot for Transport Dash, built and maintained by mesub#0556.")
+    embed.set_thumbnail(url="https://images-ext-2.discordapp.net/external/wjVGcBzfe17nPYm7mDBhmvyXyyJVgwkpYWSHCneyZBg/%3Fsize%3D256/https/cdn.discordapp.com/avatars/740568208351690803/54fff7b36442635ac6bfd3f457cec28a.png")
+    embed.set_footer(text="mesub#0556", icon_url="https://cdn.discordapp.com/embed/avatars/0.png")
+    embed.add_field(name="About", value="This is just a simple bot to make some tasks in Transport Dash a little easier. Don't expect much Functionality outside of it 😄.")
+    embed.add_field(name="Source code", value="If you want to play around with the bot or run an instance of it, then the code can be found [here](https://github.com/mesub7/pogmas)")
+    await ctx.send(embed=embed)
+    
 @bot.event
 async def on_message(message):
     channel = 703665588244971582
