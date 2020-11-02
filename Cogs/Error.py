cfac0693d38431e4bc356623e01ea7665ea5baca
David <49169805+mesub7@users.noreply.github.com>
2020-11-02 16:01:03 +0000

    Centralisation of checks.

--- Cogs/Error.py
+++ Cogs/Error.py
@@ -19,6 +19,19 @@ class CommandErrorHandler(commands.Cog):
             The Exception raised.
         """
 
+        # This allows us to call the default error handler at any time
+        async def ee():
+            print(f'Ignoring exception in command {ctx.command}:', file=sys.stderr)
+            traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+            py_error = traceback.format_exception(type(error), error, error.__traceback__)
+            py_error = ''.join(py_error)
+            channel = self.bot.get_channel(767416350552490025)
+            await channel.send(f'An error occured with command `{ctx.command}` in cog `{ctx.cog.qualified_name}`.\
+            \n The command was invoked in <#{ctx.channel.id}> by `{ctx.author}`.\nThe server this was invoked in was `{ctx.guild}`. \nJumplink to command execution: {ctx.message.jump_url} . \nException:')
+            await channel.send(f'```py\n{py_error}```')
+            embed = discord.Embed(title="⚠ An error occurred ⚠", colour=discord.Colour.red(), description="An unexpected error has occured, this should never happen. I have sent details to mesub#0556.")
+            await ctx.send(embed=embed)
+
         # This prevents any commands with local handlers being handled here in on_command_error.
         if hasattr(ctx.command, 'on_error'):
             return
@@ -46,44 +59,49 @@ class CommandErrorHandler(commands.Cog):
                 \nIf you think this is a mistake please contact `mesub#0556`.')
 
         elif isinstance(error, discord.ext.commands.errors.NotOwner):
-            if ctx.command.qualified_name in ("load", "unload", "reload", "status", "activity", "jsk py"):
+            if ctx.command.qualified_name in ('jishaku py'):
+                await ctx.send("Evaluation of **python** code can only be executed by the bot owner.")
+            else:
                 await ctx.send("Command execution failed: You are not the bot owner. (10 points for finding this command though.)")
 
         elif isinstance(error, discord.ext.commands.DisabledCommand):
-            if ctx.command.qualified_name in ("cut"):
-                await ctx.send("Command execution failed: Command is disabled and cannot be run, sorry.")
+            await ctx.send("Command execution failed: Command is disabled and cannot be run, sorry.")
+
 
         elif isinstance(error, discord.ext.commands.BadArgument):
             if ctx.command.qualified_name in ("say"):
                 await ctx.send("Command execution failed: Channel not found.")
+            else:
+                await ee()
+
+        elif isinstance(error, discord.ext.commands.errors.TooManyArguments):
+            if ctx.command.qualified_name in ("jishaku py"):
+                await ctx.send("It's either: \n`ceval`\n`cjsk py` or\n`cjishaku py`\nOKay?")
+            else:
+                await ee()
 
         elif isinstance(error, discord.ext.commands.DisabledCommand):
             if ctx.command.qualified_name in ("dm"):
                 await ctx.send("Command execution failed: User not found.")
+            else:
+                await ee()
 
         elif isinstance(error, discord.ext.commands.MissingRequiredArgument):
             await ctx.send("Command execution failed: Argument is missing! Correct usage:")
             await ctx.send_help(ctx.command)
 
         elif isinstance(error, discord.ext.commands.CommandError):
-            if ctx.qualified_name in ("say", "dm"):
+            if ctx.command.qualified_name in ("say", "dm"):
                 await ctx.send("Command execution failed: You are not in the `lvl4` group of users authorised to use this command.\
                 \nIf you think this is a mistake please contact mesub#0556.")
-            elif ctx.qualified_name in ("cut"):
+            elif ctx.command.qualified_name in ("cut"):
                 await ctx.send("Command execution failed: You are not in the `lvl3` (Boosters or channel members) group of users authorised to use this command.\
                 \nIf you think this is a mistake please contact mesub#0556.")
+            else:
+                await ee()
         else:
             # All other Errors not returned come here. And we can just print the default TraceBack.
-            print(f'Ignoring exception in command {ctx.command}:', file=sys.stderr)
-            traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
-            py_error = traceback.format_exception(type(error), error, error.__traceback__)
-            py_error = ''.join(py_error)
-            channel = self.bot.get_channel(767416350552490025)
-            await channel.send(f'An error occured with command `{ctx.command}` in cog `{ctx.cog.qualified_name}`.\
-            \n The command was invoked in <#{ctx.channel.id}> by `{ctx.author}`.\nThe server this was invoked in was `{ctx.guild}`. \nJumplink to command execution: {ctx.message.jump_url} . \nException:')
-            await channel.send(f'```py\n{py_error}```')
-            embed = discord.Embed(title="⚠ An error occurred ⚠", colour=discord.Colour.red(), description="An unexpected error has occured, this should never happen. I have sent details to mesub#0556.")
-            await ctx.send(embed=embed)
+            await ee()
 
 
 
