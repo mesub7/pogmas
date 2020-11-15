0518e019c1351e5b6c2903917a450efb6e09c43f
David <49169805+mesub7@users.noreply.github.com>
2020-11-15 15:02:17 +0000

    Add new member to lvl3 command group.

--- Cogs/Checks.py
+++ Cogs/Checks.py
@@ -9,7 +9,7 @@ def lvl3():
         return any(
         role.id in [770380094866063380, 660926272750223361, 754287737439387679, 407585313129758720, 521372852952498179]
         for role in ctx.author.roles
-        ) or ctx.author.id == 414530505585721357
+        ) or ctx.author.id in (414530505585721357, 197100324727685121)
     return commands.check(predicate)
 
 async def lvl4(ctx):
