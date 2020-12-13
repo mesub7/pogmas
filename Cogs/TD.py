1119f8acf12e5f2f84ada9c42922d0439286e0fb
David <49169805+mesub7@users.noreply.github.com>
2020-12-13 19:12:23 +0000

    Brexit means brexit.

--- Cogs/TD.py
+++ Cogs/TD.py
@@ -1,6 +1,7 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 import asyncio
+import datetime
 
 class TD(commands.Cog, command_attrs=dict(hidden=True)):
     def __init__(self, bot):
@@ -16,5 +17,18 @@ class TD(commands.Cog, command_attrs=dict(hidden=True)):
             await asyncio.sleep(1)
             await message.add_reaction(emoji1)
 
+    @tasks.loop(hours=24)
+    async def auto_brexit(self):
+        channel = self.bot.get_channel(395956678412992523)
+        dt  = datetime.datetime
+        now = dt.now()
+        cd=dt(year=2021, month=1,day=1) - dt(year=now.year, month=now.month, day=now.day)
+        await channel.send(f'There are `'+str(cd)[:str(cd).find(",")]+'` until <:BR:772541846357278791><:EX:772541846358196254><:IT:756911540728496220>')
+
+    @auto_brexit.before_loop
+    async def before_brexit(self):
+        await self.bot.wait_until_ready()
+
+    auto_brexit.start()
 def setup(bot):
     bot.add_cog(TD(bot))
