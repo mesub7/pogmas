dff4e3cd71e671376ab2fc4e3ea8c0b35b821e2b
David <49169805+mesub7@users.noreply.github.com>
2020-12-23 17:20:54 +0000

    This wasn't commited? Fix on_message

--- Cogs/TD.py
+++ Cogs/TD.py
@@ -12,32 +12,38 @@ class TD(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        channel = 703665588244971582
-        emoji = '\N{THUMBS UP SIGN}'
-        emoji1 = '\N{THUMBS DOWN SIGN}'
-        emoji2 = 'a:siren:789554809006325760'
-        role1 = message.guild.get_role(703230475472207924)
-        if channel == int(message.channel.id):
-            await message.add_reaction(emoji)
-            await asyncio.sleep(1)
-            await message.add_reaction(emoji1)
-        if role1.mention in message.content.lower():
-            await message.channel.send("<a:siren:789554809006325760> **Staff on duty have been notified, please stand by.** <a:siren:789554809006325760>")
-            await message.add_reaction(emoji2)
-            await role.edit(mentionable=False, reason="Staff on duty role mentioned.")
-            await asyncio.sleep(120)
-            await role.edit(mentionable=True, reason="Cooldown reached - Role can be mentioned again.")
+        if message.guild.id == 322061619570016256:
+            channel = 703665588244971582
+            emoji = '\N{THUMBS UP SIGN}'
+            emoji1 = '\N{THUMBS DOWN SIGN}'
+            if message.channel.id == channel:
+                await message.add_reaction(emoji)
+                await asyncio.sleep(1)
+                await message.add_reaction(emoji1)
 
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        role = discord.Role
-        role2 = message.guild.get_role(790631558976503830)
-        if 653685492100890635 == message.channel.id and ("#QoTD") in message.content:
-            if any(
-            role.id in [665930105909936139, 407585313129758720, 521372852952498179] #Questioner, Sever manager, Owner
-            for role in message.author.roles
-            ):
-                await message.channel.send(role2.mention)
+    @commands.Cog.listener('on_message')
+    async def SoD(self, message):
+        if message.guild.id == 322061619570016256:
+            emoji2 = 'a:siren:789554809006325760'
+            role1 = message.guild.get_role(703230475472207924)
+            if role1.mention in message.content.lower():
+                await message.channel.send("<a:siren:789554809006325760> **Staff on duty have been notified, please stand by.** <a:siren:789554809006325760>")
+                await message.add_reaction(emoji2)
+                await role.edit(mentionable=False, reason="Staff on duty role mentioned.")
+                await asyncio.sleep(120)
+                await role.edit(mentionable=True, reason="Cooldown reached - Role can be mentioned again.")
+
+    @commands.Cog.listener('on_message')
+    async def QoTD(self, message):
+        if message.guild.id == 322061619570016256:
+            role = discord.Role
+            role2 = message.guild.get_role(790631558976503830)
+            if 653685492100890635 == message.channel.id and ("#QoTD") in message.content:
+                if any(
+                role.id in [665930105909936139, 407585313129758720, 521372852952498179] #Questioner, Sever manager, Owner
+                for role in message.author.roles
+                ):
+                    await message.channel.send(role2.mention)
 
     @tasks.loop(hours=24)
     async def auto_brexit(self):
@@ -79,7 +85,7 @@ class TD(commands.Cog):
     async def opt_out(self, ctx, *, reason):
         def check(reaction, user):
             return reaction.emoji == '👍' and user == ctx.author
-        msg = await ctx.send("Are you sure you want to opt out of having your cut liked? This is a **one time** process and mesub will be annoyed if he has to remove you from this list.")
+        msg = await ctx.send("Are you sure you want to opt out of having your cut liked? This is a **one time** process and mesub will be annoyed if he has to remove you from this list.\n```\nYou also agree that you will not be able to like other people's cuts.\n```")
         await msg.add_reaction('👍')
         await msg.add_reaction('👎')
         try:
@@ -87,7 +93,7 @@ class TD(commands.Cog):
         except asyncio.TimeoutError:
             await ctx.send("I don't have all day so I assumed you said no.")
             return
-        await ctx.send("Ok, I've sent your ID and reason to mesub, he will have a look at it and approve it if needed.")
+        await ctx.send("Ok, I've sent your ID and reason to mesub. You will be updated in your DMs, you make sure they're on.")
         channel = self.bot.get_channel(790618543904915486)
         await channel.send(f"__**New request from: {str(ctx.author)}**__\nThe reason is:\n`{reason}`\nTheir ID is:")
         await channel.send(f"{ctx.author.id}")
