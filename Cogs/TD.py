e28f65205d246230921adc617d66361a40977273
David <49169805+mesub7@users.noreply.github.com>
2021-02-28 17:17:06 +0000

    Add opt-in command.

--- Cogs/TD.py
+++ Cogs/TD.py
@@ -32,11 +32,11 @@ class TD(commands.Cog):
             emoji2 = 'a:siren:789554809006325760'
             role1 = message.guild.get_role(703230475472207924)
             if role1.mention in message.content.lower():
-                await message.channel.send("<a:siren:789554809006325760> **Staff on duty have been notified, please stand by.** <a:siren:789554809006325760>")
+                await message.channel.send('<a:siren:789554809006325760> **Staff on duty have been notified, please stand by.** <a:siren:789554809006325760>')
                 await message.add_reaction(emoji2)
-                await role.edit(mentionable=False, reason="Staff on duty role mentioned.")
+                await role.edit(mentionable=False, reason='Staff on duty role mentioned.')
                 await asyncio.sleep(120)
-                await role.edit(mentionable=True, reason="Cooldown reached - Role can be mentioned again.")
+                await role.edit(mentionable=True, reason='Cooldown reached - Role can be mentioned again.')
 
     @commands.Cog.listener('on_message')
     async def QoTD(self, message):
@@ -45,7 +45,7 @@ class TD(commands.Cog):
         if message.guild.id == 322061619570016256:
             role = discord.Role
             role2 = message.guild.get_role(790631558976503830)
-            if 653685492100890635 == message.channel.id and ("#QoTD") in message.content:
+            if 653685492100890635 == message.channel.id and ('#QoTD') in message.content:
                 if any(
                 role.id in [665930105909936139, 407585313129758720, 521372852952498179] #Questioner, Sever manager, Owner
                 for role in message.author.roles
@@ -70,7 +70,7 @@ class TD(commands.Cog):
         now = dt.now()
         channel = self.bot.get_channel(720709557445460028)
         if now.weekday() == 5:
-            await channel.send("<@414530505585721357> Don't forget to ask about questioners!")
+            await channel.send('<@414530505585721357> Don\'t forget to ask about questioners!')
 
     @quest_enq.before_loop
     async def before_qe(self):
@@ -82,30 +82,53 @@ class TD(commands.Cog):
         now = dt.now()
         channel = self.bot.get_channel(720709557445460028)
         if now.weekday() == 6:
-            await channel.send("<@414530505585721357> Don't forget to pick a questioner!")
+            await channel.send('<@414530505585721357> Don\'t forget to pick a questioner!')
 
     @quest_pick.before_loop
     async def before_pick(self):
         await self.bot.wait_until_ready()
 
-    @commands.command(aliases=["oo"], help="Would you like to not have your cut liked?", description="Your reason should be short and sweet.")
+    @commands.command(aliases=["oo"], help='Would you like to not have your cut liked?', description='Your reason should be short and sweet.')
     async def opt_out(self, ctx, *, reason):
         def check(reaction, user):
             return user == ctx.author
-        msg = await ctx.send("Are you sure you want to opt out of having your cut liked? This is a **one time** process and mesub will be annoyed if he has to remove you from this list.\n```\nYou also agree that you will not be able to like other people's cuts.\n```")
+        msg = await ctx.send('Are you sure you want to opt out of having your cut liked? This isn\'t a **light** decision and mesub will be (now less) annoyed if he has to remove you from this list.\n```\nYou also agree that you will not be able to like other people\'s cuts.\n```')
         await msg.add_reaction('👍')
         await msg.add_reaction('👎')
         try:
             reaction, user = await self.bot.wait_for('reaction_add', check=check, timeout=20.0)
         except asyncio.TimeoutError:
-            await ctx.send("I don't have all day so I assumed you said no.")
+            await ctx.send('I don\'t have all day so I assumed you said no.')
             return
         if str(reaction.emoji) == '👎':
-            await ctx.send("Oh, you changed your mind? Alrighty.")
+            await ctx.send('Oh, you changed your mind? Alrighty.')
             return
-        await ctx.send("Ok, I've sent your ID and reason to mesub. You will be updated in your DMs, you make sure they're on.")
+        await ctx.send('Ok, I\'ve sent your ID and reason to mesub. You will be updated in your DMs, you make sure they\'re on.')
         channel = self.bot.get_channel(790618543904915486)
-        await channel.send(f"__**New request from: {str(ctx.author)}**__\nThe reason is:\n`{reason}`\nTheir ID is:")
+        await channel.send(f'__**New request from: {str(ctx.author)}**__\nThe reason is:\n`{reason}`\nTheir ID is:')
+        await channel.send(f"{ctx.author.id}")
+
+    @commands.command(aliases=['oi'], help='Want to have your cut liked again?', description='Your reason should be a little longer and sweeter.')
+    async def opt_in(self, ctx, *, reason):
+        def check(reaction, user):
+            return user == ctx.author
+        if ctx.author.id not in self.bot.no_cut:
+            await ctx.send('Why do you need to opt in? You\'ve not opted out from my records...')
+            return
+        msg = await ctx.send('Are you sure you want to opt back in to having your cut liked? This isn\'t a **light** decision and you won\'t be able to just opt out again.\n```\nIt does mean you can like other people\'s cuts again.\n```')
+        await msg.add_reaction('👍')
+        await msg.add_reaction('👎')
+        try:
+            reaction, user = await self.bot.wait_for('reaction_add', check=check, timeout=20.0)
+        except asyncio.TimeoutError:
+            await ctx.send('I don\'t have all day so I assumed you said no.')
+            return
+        if str(reaction.emoji) == '👎':
+            await ctx.send('Oh, you changed your mind? Alrighty.')
+            return
+        await ctx.send('Ok, I\'ve sent your ID and reason to mesub. You will be updated in your DMs, you make sure they\'re on.')
+        channel = self.bot.get_channel(790618543904915486)
+        await channel.send(f'__**New **opt in** request from: {str(ctx.author)}**__\nThe reason is:\n`{reason}`\nTheir ID is:')
         await channel.send(f"{ctx.author.id}")
 
 def setup(bot):
